6e7d5abd595965915f5a1f997ce317a50733a423
d-martirosyan <david.martirosyan.2112@gmail.com>
2025-03-07 16:26:08 +0400

    Whatsapp

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="arm">
+      <head>
+        <meta property="og:title" content="Էրիկ և Լանա" />
+        <meta property="og:description" content="Հրավեր" />
+        <meta property="og:image" content="/assets/images/glavniy.PNG" />
+        <meta property="og:url" content="https://erik-lana.vercel.app" />
+      </head>
       <body>
         {children}
       </body>
