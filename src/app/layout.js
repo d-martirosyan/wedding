737be47acf33d47b3fcdc3650310813854071660
d-martirosyan <david.martirosyan.2112@gmail.com>
2025-03-07 16:55:23 +0400

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,14 +9,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="arm">
       <head>
-        <meta property="og:title" content="Էրիկ և Լանա" />
-        <meta property="og:description" content="Հրավեր" />
-        <meta property="og:image" content="/assets/images/main.png" />
-        <meta property="og:url" content="https://erik-lana.vercel.app" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Էրիկ և Լանա" />
         <meta name="twitter:description" content="Հրավեր" />
         <meta name="twitter:image" content="/assets/images/main.png" />
+        <meta property="og:title" content="Էրիկ և Լանա" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://erik-lana.vercel.app" />
+        <meta property="og:image:url" content="/assets/images/main.png" />
+        <meta property="og:description" content="Հրավեր" />
       </head>
       <body>
         {children}
